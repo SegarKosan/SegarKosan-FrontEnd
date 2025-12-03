08db282d64137ced9952b6ff8bbe0ee037cd2f37
Rizal Kalam <rizalkalam78@gmail.com>
2025-12-03 21:01:15 +0700

    change websocket configure

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://17036dcd4c93.ngrok-free.app"; // backend Express
+const API_URL = "https://segarkosan-backend.vercel.app"; // backend Express
 
 export default axios.create({
   baseURL: API_URL,
